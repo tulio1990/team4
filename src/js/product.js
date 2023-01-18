e9da6a79bdc7a2077c5c4029a947d400357de659
JoseAndresGGiron <titogg@byui.edu>
2023-01-17 21:40:03 -0500

    committing till step# 5. We still need to finish step# 6.

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,6 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
