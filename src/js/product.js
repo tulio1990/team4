17204deea0e3ad1e7d06547e3ba1b970358f27bd
tulio1990 <trbanegas14@gmail.com>
2023-01-21 21:13:17 -0500

    Update Class

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,9 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-
-const dataSource = new ProductData('tents');
-
+const dataSource = new ProductData("tents");
 const productId = getParam("product");
-console.log(dataSource.findProductById(productId));
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
