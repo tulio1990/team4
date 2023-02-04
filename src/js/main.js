cd546c9f2a039c134f9f647dbfd9097129178505
tulio1990 <trbanegas14@gmail.com>
2023-02-03 23:40:25 -0600

    tb Product List sort

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,44 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("tents", dataSource, element);
+const selection = document.getElementById("select-sort");
+// const selectO = new SelectOption("Name","Price", selection);
+// selectO.init();
+const btn = document.getElementById("btn");
 
-listing.init();
+btn.onclick = (event) => {
+    event.preventDefault();
+    // show the selected index
+    if(selection.selectedIndex == 0){
+        const dataSource = new ProductData("tents");
+        const element = document.querySelector(".product-list");
+        const listing = new ProductList("tents", dataSource, element, "Name");
+
+        listing.init();
+    }else{
+        const dataSource = new ProductData("tents");
+        const element = document.querySelector(".product-list");
+        const listing = new ProductList("tents", dataSource, element, "FinalPrice");
+
+        listing.init();
+    };
+};
+
+if(selection.selectedIndex == 0){
+    const dataSource = new ProductData("tents");
+    const element = document.querySelector(".product-list");
+    const listing = new ProductList("tents", dataSource, element, "Name");
+
+    listing.init();
+}else{
+    const dataSource = new ProductData("tents");
+    const element = document.querySelector(".product-list");
+    const listing = new ProductList("tents", dataSource, element, "FinalPrice");
+
+    listing.init();
+};
+
+
 
 loadHeaderFooter();
+
