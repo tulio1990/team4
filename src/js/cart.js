4b23b4322942611ef3f695965e0ce623917c4c5b
nefierez <nefierez@byui.edu>
2023-02-21 18:13:34 -0600

    Running the Lint & Format scripts

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,4 @@
-import {
-  getLocalStorage,
-  loadHeaderFooter,
-  itemsInBackpack
-} from "./utils.mjs";
+import { loadHeaderFooter, itemsInBackpack } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter().then(() => itemsInBackpack());
