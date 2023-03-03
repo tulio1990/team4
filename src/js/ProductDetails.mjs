01214f3b885530e47d7144b75491f4d7bc6c11b8
JoseAndresGGiron <titogg@byui.edu>
2023-03-02 23:42:22 -0500

    Product Comments Done! No errors Found

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,7 +1,11 @@
-import { setLocalStorage, getLocalStorage, itemsInBackpack, addClass } from "./utils.mjs";
+import {
+  setLocalStorage,
+  getLocalStorage,
+  itemsInBackpack,
+  addClass
+} from "./utils.mjs";
 
 function productDetailsTemplate(product) {
-  const discount = ((product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice) * 100;
   return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
     <h2 class="divider">${product.NameWithoutBrand}</h2>
     <picture>
@@ -12,16 +16,25 @@ function productDetailsTemplate(product) {
       alt="${product.NameWithoutBrand}"
     />
     <picture>
-    <p class="product-card__price">$${(product.FinalPrice).toFixed(2)}</p>
-    <p class="product-card__discount">${(discount).toFixed(0)}% off</p>
-    <p class="product-card__RegPrice">Reg: $${(product.SuggestedRetailPrice).toFixed(2)}</p>
+    <p class="product-card__price">$${product.FinalPrice}</p>
     <p class="product__color">${product.Colors[0].ColorName}</p>
     <p class="product__description">
     ${product.DescriptionHtmlSimple}
     </p>
     <div class="product-detail__add">
       <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    </div></section>`;
+    </div>
+    <div class="product-detail__comments">
+      <h3 id="h3Comments">Comments</h3>
+      <ul class="comment-list"></ul>
+      <form class="comment-form">
+        <label for="comment">Add a comment:</label>
+
+        <textarea id="comment" name="comment"></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    </section>`;
 }
 
 export default class ProductDetails {
@@ -29,42 +42,89 @@ export default class ProductDetails {
     this.productId = productId;
     this.product = {};
     this.dataSource = dataSource;
-    this.discount = "";
+    this.comments = [];
+    this.commentList = null;
+    this.commentForm = null;
   }
+
   async init() {
-    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
-
-    console.log(this.product);
-    console.log(this.productId);
-
-    // once we have the product details we can render out the HTML
-    this.renderProductDetails("main");
-    // once the HTML is rendered we can add a listener to Add to Cart button
-    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-    document
-      .getElementById("addToCart")
-      .addEventListener("click", this.addToCart.bind(this));
+    this.renderProductDetails();
+    document.getElementById("addToCart").addEventListener("click", this.addToCart.bind(this));
+    this.commentList = document.querySelector(".comment-list");
+    this.commentForm = document.querySelector(".comment-form");
+    this.loadComments();
+    this.commentForm.addEventListener("submit", this.handleCommentSubmit.bind(this));
   }
+
+  renderProductDetails() {
+    const element = document.querySelector("main");
+    element.insertAdjacentHTML(
+      "afterBegin",
+      `<section class="product-detail">
+        <h3>${this.product.Brand.Name}</h3>
+        <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+        <picture>
+          <source media="(max-width: 499px)" srcset="${this.product.ImageSmall}">
+          <img class="divider" src="${this.product.Images.PrimaryLarge}" alt="${this.product.NameWithoutBrand}">
+        </picture>
+        <p class="product-card__price">$${this.product.FinalPrice}</p>
+        <p class="product__color">${this.product.Colors[0].ColorName}</p>
+        <p class="product__description">${this.product.DescriptionHtmlSimple}</p>
+        <div class="product-detail__add">
+          <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+        </div>
+        <div class="product-detail__comments">
+          <h3 id="h3Comments">Comments</h3>
+          <ul class="comment-list"></ul>
+          <form class="comment-form">
+            <label for="comment">Add a comment:</label>
+            
+            <textarea id="comment" name="comment"></textarea>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </section>`
+    );
+  }
+
   addToCart() {
     let cartContents = getLocalStorage("so-cart");
-    let cartIcon = document.querySelector(".cart");//find the Cart icon
-    addClass(cartIcon, "sproing");//make the cart icon jiggle
-    //check to see if there was anything there
+    let cartIcon = document.querySelector(".cart");
+    addClass(cartIcon, "sproing");
     if (!cartContents) {
       cartContents = [];
     }
-    // then add the current product to the list
     cartContents.push(this.product);
     setLocalStorage("so-cart", cartContents);
-    alert("Great! Your item has been added to the cart")
+    alert("Great! Your item has been added to the cart");
     itemsInBackpack();
   }
-  renderProductDetails(selector) {
-    const element = document.querySelector(selector);
-    element.insertAdjacentHTML(
-      "afterBegin",
-      productDetailsTemplate(this.product)
-    );
+
+  loadComments() {
+    const comments = getLocalStorage(`product-${this.productId}-comments`);
+    if (comments) {
+      this.comments = comments;
+      this.renderComments();
+    }
+  }
+
+  handleCommentSubmit(event) {
+    event.preventDefault();
+    const commentInput = document.getElementById("comment");
+    const comment = commentInput.value;
+    this.comments.push(comment);
+    setLocalStorage(`product-${this.productId}-comments`, this.comments);
+    this.renderComments();
+    commentInput.value = "";
+  }
+
+  renderComments() {
+    this.commentList.innerHTML = "";
+    for (const comment of this.comments) {
+      const li = document.createElement("li");
+      li.innerText = comment;
+      this.commentList.appendChild(li);
+    }
   }
 }
