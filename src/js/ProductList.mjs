0ddced5b479533add835bd1092298a6e89023957
nefierez <nefierez@byui.edu>
2023-03-04 00:08:49 -0600

    Updating everything from QuickLookup2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -11,12 +11,17 @@ function productCardTemplate(product) {
       src="${product.Images.PrimaryMedium}"
       alt="Image of ${product.Name}"
     />
-    <picture>
+    <picture></a>
+    <section class="product-card-buttons">
+      <button><img class="card-buttons" id="${product.Id}" src="../images/icon_watch.svg" alt="icon for lookup"></button>
+      <button><img class="card-buttons" id="${product.SuggestedRetailPrice}" src="../images/icon_comment.svg" alt="icon for comment"></button>
+      <button><img class="card-buttons" id="${product.FinalPrice}" src="../images/icon_wish.svg" alt="icon for wish"></button>
+    </section>
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.Name}</h2>
     <p class="product-card__price">$${(product.FinalPrice).toFixed(2)}</p>
     <p class="product-card__discount">${(discount).toFixed(0)}% off</p>
-    <p class="product-card__RegPrice">Reg: $${(product.SuggestedRetailPrice).toFixed(2)}</p></a>
+    <p class="product-card__RegPrice">Reg: $${(product.SuggestedRetailPrice).toFixed(2)}</p>
   </li>`;
   }
 
@@ -54,11 +59,21 @@ export default class ProductList {
   // render after doing the first stretch
   renderList(list) {
     renderListWithTemplate(productCardTemplate, this.listElement, list);
+    this.gettingIDs();
   }
-
-  // render before doing the stretch
-//   renderList(list) {
-//     const htmlStrings = list.map(productCardTemplate);
-//     this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-//   }
+  gettingIDs(){
+    const card_buttons = document.querySelectorAll(".card-buttons");
+    card_buttons.forEach((card_button) => {
+      card_button.addEventListener("click", (event) => {
+        const ModalId = event.target.id;
+        console.log(ModalId);
+        this.displayingModal(ModalId);
+      })
+    })
+  }
+  async displayingModal(ModalId){
+    const productModal = await this.dataSource.findProductById(ModalId);
+      console.log(productModal);
+      alert("Thank you!");
+  };
 }
